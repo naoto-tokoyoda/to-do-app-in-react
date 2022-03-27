8419ea4252f9d023f9df9915ca72d31dbfefeaff
Naoto Tokoyoda <tkyd89@gmail.com>
2022-03-26 17:11:42 -0700

    responsive design is attached

--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -5,6 +5,7 @@ import './Button.css';
 
 const Button = styled.button`
 & {
+  width:100%;
   font: inherit;
   padding: 0.5rem 1.5rem;
   border: 1px solid #8b005d;
@@ -13,8 +14,9 @@ const Button = styled.button`
   box-shadow: 0 0 4px rgba(0, 0, 0, 0.26);
   cursor: pointer;
 
+  // below 768px, these style will work
   @media(min-width:768px){
-    background-color:pink;
+    width:auto;
   }
 
 }
